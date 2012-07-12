acf8f9feec55e376eee5a898c12b09f348f395d5
Markus Düttman <duettman@owens.rz-berlin.mpg.de>
2012-07-12 11:19:49 +0200

    Network: Define Node and Link classes

--- community-detection.rb
+++ community-detection.rb
@@ -1,7 +1,24 @@
+class Node
+	attr_accessor :id, :spin
+	def initialize(id,spin)
+		@id	  = id
+		@spin = spin
+	end
+end
+
+class Link
+	attr_accessor :id, :node1, :node2
+	def initialize(node1,node2)
+		@node1 = node1
+		@node2 = node2
+	end
+end
+
+
 class Network
-	 attr_accessor :nodes, :edges
+	attr_accessor :nodes, :edges
 	 
-	 def initialize(file_name)
+	def initialize(file_name)
 		@nodes = []
 		@edges = []
 		file = File.new(file_name, "r")
@@ -9,9 +26,9 @@ class Network
 			if line[0].chr!="#"
 				a =  line.split("\t")
 				if @nodes[-1]!= a[0]
-					@nodes << a[0]
+					@nodes << Node.new(a[0],0)
 				end
-				@edges << a
+				@edges << Link.new(a[0],a[1])
 			end
 		end
 		file.close
@@ -21,4 +38,7 @@ end
 
 test = Network.new("email-Enron.txt")
 
-puts test.nodes
+test.nodes.each do |node|
+	puts "#{node.id} : #{node.spin}"
+
+end
