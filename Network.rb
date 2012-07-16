93b5b7ad33687c58ec927547ad502959f06b6585
Markus Düttmann <duettmann@gmail.com>
2012-07-16 21:52:12 +0200

    Error in simulated-annealing line 88

--- Network.rb
+++ Network.rb
@@ -24,29 +24,33 @@ class Network
 		@groups 	= Array.new(q){[]}
 		@q 			= q					## number of groups
 		file = File.new(file_name, "r")
+		@nodes << Node.new(0,rand(@q))
 		while (line = file.gets)
 			if line[0].chr!="#"
 				a =  line.split("\t")
-				if @nodes[-1]!= a[0]
+				if @nodes.size == 0 || @nodes[-1].id != a[0].to_i
 					@nodes << Node.new(a[0].to_i,rand(@q))
 				end
 				@edges << Link.new(a[0].to_i,a[1].to_i)
 				@neighbors[a[1].to_i] = []
 			end
 		end
+	#	@nodes.uniq
 		file.close
 		@adjacency  = Array.new(@nodes.size){Array.new(@nodes.size){0}}
 		@edges.each do |link| 
-			@neighbors[link.node2] << link.node1
+			@neighbors[link.node1] << link.node2
 			@adjacency[link.node1][link.node2]=1
 		end
+
 		@nodes.each do |node|
+	#		puts "#{node.spin} : #{node.id}"
 			@groups[node.spin] << node.id
 		end
 
 	end
 	def self.lottery()
-		@groups 	= Array.new(){[]}
+		@groups 	= Array.new(q){[]}
 		@nodes.each do |node| 
 			node.spin = rand(@q)
 			@groups[node.spin] << node.id
--- simulated-annealing.rb
+++ simulated-annealing.rb
@@ -3,9 +3,9 @@ require "Network"
 
 # Initial values
 TEST		=true
-PROB_RESTART=0.001
+PROB_RESTART=0
 TIME_MAX	= 30000
-MAX_ENERGY	= 0
+MAX_ENERGY	= -100000
 ALPHA		= 0.95
 GAMMA 		= 1
 NULL_P 		= 0.1
@@ -14,36 +14,35 @@ class State < Network
 	attr_accessor :energy, :p, :gamma
 	# energy calculates engery of state
 	# neighbor gives random neighboring state (i.e. 1 spin turned)
-	def initialize(file_name, q, gamma,p)
+	def initialize(file_name, q, gamma)
 		super(file_name,q)
-		@p = p
+		@p = @edges.size/@nodes.size
 		@gamma = gamma
 		@energy = 0
 		@edges.each do |link|
 			@energy -= (@adjacency[link.node1][link.node2]-@gamma*@p) 
 		end
 	end
-
-
 	def neighbor_state()
-		old_spin = @nodes[pick_id].spin
+		rand_id = rand(@nodes.size)
+		old_spin = @nodes[rand_id].spin
 		begin 
 			new_spin	= rand(@q)
-		end while @nodes[pick_id].spin == new_spin
-		@nodes[pick_id].spin = new_spin
+		end while @nodes[rand_id].spin == new_spin
+		@nodes[rand_id].spin = new_spin
 		## energy update
 		## calculate adhesions: null model = links established with equal probability p
-		a_l_phi = @groups[old_spin].inject{|result, element| result + @adjacency[pick_id][element]}
-		a_l_phi = a_l_phi - @gamma*@p*(@group[old_spin]-1)
-		a_l_alpha = @groups[new_spin].inject([]) do |result, element|
-  			result + @adjacency[pick_id][element]
-  			result
+		a_l_phi = @groups[old_spin].inject{|result, element| result + @adjacency[rand_id][element]}
+		a_l_phi = a_l_phi - @gamma*@p*(@groups[old_spin].size-1)
+		a_l_alpha = 0
+		@groups[new_spin].each do |i| 
+			a_l_alpha += @adjacency[rand_id][i]
 		end
-		a_l_alpha = a_l_alpha - @gamma*@p*(@group[new_spin])
+		a_l_alpha -= @gamma*@p*(@groups[new_spin].size)
 
-		@energy = @energy + a_l_phi - a_l_alpha
-		@groups[old_spin].drop(pick_id)
-		@groups[new_spin] << pick_id
+		@energy += a_l_phi - a_l_alpha
+		@groups[old_spin].drop(rand_id)
+		@groups[new_spin] << rand_id
 	end
 
 end
@@ -66,14 +65,13 @@ def boltzmann(e, e_new, temp)
   Math.exp((e_new-e) * (1.0 - temp))
 end
 
-
 def simulated_annealing( initial,
 		prob_restart 	= PROB_RESTART,
 		time_max		= TIME_MAX,
 		max_energy 		= MAX_ENERGY,
-		temp			= temp(alpha),
-		distribution   	= method(:boltzmann))
-	
+		temp			= temp(ALPHA),
+		distribution 	= method(:boltzmann))
+		
 	t  				= 0
 	t_after_restart = 0
 	current			= initial
@@ -85,9 +83,11 @@ def simulated_annealing( initial,
 			current.lottery()
 		else
 			new_state  = current
-			new_state  = new_state.neighbor_state()
+			new_state.neighbor_state()
+			puts "|||||\n"
+			puts distribution.call(current.energy, new_state.energy, temp)
 		end
-		if distribution(current.energy,new_state.energy,temp) > rand
+		if distribution.call(current.energy, new_state.energy, temp) > rand
 			current = new_state
 		end
 		if current.energy < best.energy
